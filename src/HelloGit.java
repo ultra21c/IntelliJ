d6f6cb304c6fc36e2825bc64dd56e40356378376
Jung PilWon <ultra21c@gmail.com>
2014-07-06 00:58:44 +0900

    readme.me

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit");
-        System.out.println("HelloGit22");
+        System.out.println("HelloGit2");
     }
 }
